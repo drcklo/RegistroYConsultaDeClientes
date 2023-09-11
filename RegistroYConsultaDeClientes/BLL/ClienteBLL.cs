9ec56deec772391f28970c5c2e02db795d7531b4
darcklito <luis_paulino5@ucne.edu.do>
2023-09-11 15:31:11 -0400

    Inyeccion de contexto a la BLL

--- RegistroYConsultaDeClientes/BLL/ClienteBLL.cs
+++ RegistroYConsultaDeClientes/BLL/ClienteBLL.cs
@@ -1,6 +1,13 @@
-﻿namespace RegistroYConsultaDeClientes.BLL
+﻿using RegistroYConsultaDeClientes.DAL;
+
+namespace RegistroYConsultaDeClientes.BLL
 {
     public class ClienteBLL
     {
+        private Contexto _contexto;
+        public ClienteBLL(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
     }
 }
