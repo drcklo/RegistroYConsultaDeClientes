66b6fc094c46101e91ba289806debc9114299b9c
darcklito <luis_paulino5@ucne.edu.do>
2023-09-11 15:41:27 -0400

    Metodos Buscar Eliminar Guardar Modificar Insertar y Existe agregados

--- RegistroYConsultaDeClientes/BLL/ClienteBLL.cs
+++ RegistroYConsultaDeClientes/BLL/ClienteBLL.cs
@@ -1,4 +1,6 @@
-﻿using RegistroYConsultaDeClientes.DAL;
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroYConsultaDeClientes.DAL;
+using RegistroYConsultaDeClientes.Model;
 
 namespace RegistroYConsultaDeClientes.BLL
 {
@@ -8,6 +10,41 @@ namespace RegistroYConsultaDeClientes.BLL
         public ClienteBLL(Contexto contexto)
         {
             _contexto = contexto;
+        }
+
+        public bool Existe(int? clientesId)
+        {
+            return _contexto.Clientes
+                .Any(e => e.ClienteId == clientesId);
+        }
+        private bool Insertar(Cliente cliente)
+        {
+            _contexto.Clientes.Add(cliente);
+            return _contexto.SaveChanges() > 0;
+        }
+        private bool Modificar(Cliente cliente)
+        {
+            _contexto.Entry(cliente).State = EntityState.Modified;
+            return _contexto.SaveChanges() > 0;
+        }
+        public bool Guardar(Cliente cliente)
+        {
+            if (!Existe(cliente.ClienteId))
+                return Insertar(cliente);
+            else
+                return Modificar(cliente);
+        }
+        public bool Eliminar(int? clientesId)
+        {
+            _contexto.Entry(clientesId).State = EntityState.Deleted;
+            return _contexto.SaveChanges() > 0;
+        }
+        public Cliente? Buscar(int? clientesId)
+        {
+            return _contexto.Clientes
+                .Where(o => o.ClienteId == clientesId)
+                .AsNoTracking()
+                .SingleOrDefault();
         }
     }
 }
