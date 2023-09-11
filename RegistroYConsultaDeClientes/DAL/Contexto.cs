97f8ace87912205e19215a199d6dc7207ab20fb5
darcklito <luis_paulino5@ucne.edu.do>
2023-09-11 14:43:51 -0400

    Contexto creado e instalacion de EntityFramework

---
+++ RegistroYConsultaDeClientes/DAL/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroYConsultaDeClientes.Model;
+using System.Collections.Generic;
+    namespace RegistroYConsultaDeClientes.DAL
+{
+    public class Contexto: DbContext
+    {
+public DbSet<Cliente> Clientes { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> options): base(options)
+        {
+        }
+    }
+}
