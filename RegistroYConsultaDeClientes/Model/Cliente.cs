143982da5227ad6765b425cc27a6bbd549426c52
darcklito <luis_paulino5@ucne.edu.do>
2023-09-11 14:19:18 -0400

    Creacion de modelo de clientes

---
+++ RegistroYConsultaDeClientes/Model/Cliente.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroYConsultaDeClientes.Model
+{
+    public class Cliente
+    {
+        [Required(ErrorMessage = "El campo ClienteId es obligatorio.")]
+        public int ClienteId { get; set; }
+
+        [Required(ErrorMessage = "El campo Nombres es obligatorio.")]
+        public string Nombres { get; set; }
+
+        [Required(ErrorMessage = "El campo Telefono es obligatorio.")]
+        public string Telefono { get; set; }
+
+        [Required(ErrorMessage = "El campo Celular es obligatorio.")]
+        public string Celular { get; set; }
+
+        [Required(ErrorMessage = "El campo RNC es obligatorio.")]
+        public string RNC { get; set; }
+
+        [Required(ErrorMessage = "El campo Email es obligatorio.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "El campo Direccion es obligatorio.")]
+        public string Direccion { get; set; }
+
+    }
+}
